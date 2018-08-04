9bc3de0e170d26f39f14ee398d2bb6a5bed426b7
yuitora <30695483+shintaroDesu@users.noreply.github.com>
2018-08-04 16:43:44 +0200

    update mergeSortedLIst, not finisehd though

---
+++ mergeSortedLIst.cpp
@@ -0,0 +1,120 @@
+#include <iostream>
+#include <vector>
+#include <list>
+
+using namespace std;
+
+struct ListNode
+{
+	int val;
+	ListNode *next;
+	ListNode(int x) : val(x), next(NULL){}
+};
+
+void print(ListNode *lists)
+{
+	cout << "lists: ";
+	while (lists != NULL)
+	{
+		cout << lists->val << " ";
+		lists = lists->next;
+	}
+	cout << "\n";
+}
+class Solution {
+private:
+	ListNode *head = NULL;
+	ListNode *tail = NULL;
+
+public:
+
+	void add_one(int min) {
+		ListNode *tmp = new ListNode(min);
+		if (head == NULL)
+		{
+			head = tmp;
+			tail = tmp;
+		}
+		else
+		{
+			tail->next = tmp;
+			tail = tail->next;
+		}
+		//delete tmp;
+
+	}
+
+	ListNode* mergeKLists(vector<ListNode*>& lists) {
+		while(lists.size() > 0) {
+
+			int min = lists[0]->val;
+			int smallestIndexNode = 0;
+
+			for(int x = 0; x < lists.size(); x++) {
+				int value = lists[x]->val;
+				if(min > lists[x]->val) {
+					smallestIndexNode = x;
+					min = lists[x]->val;
+				}
+			}
+			add_one(min);
+			lists[smallestIndexNode] = lists[smallestIndexNode]->next;
+			if (lists[smallestIndexNode] == NULL)
+				lists.erase(lists.begin() + smallestIndexNode);
+		}
+		return head;
+	}
+
+};
+
+class Linked_list {
+public:
+	ListNode *head, *tail;
+	Linked_list(int a, int b, int c = -1)
+	{
+		head = NULL;
+		tail = NULL;
+		add_one(a);
+		add_one(b);
+		if(c != -1)
+			add_one(c);
+
+	}
+
+	void add_one(int p) {
+		ListNode *tmp = new ListNode(p);
+
+		if(head == NULL) {
+			head = tmp;
+			tail = tmp;
+		} else {
+			tail->next = tmp;
+			tail = tail->next;
+		}
+	}
+
+	void print()
+	{
+		cout << head->val << "\n";
+	}
+};
+
+int main() {
+	vector<ListNode*> lists(3);
+
+	Linked_list l1(1,2,3);
+	Linked_list l2(69,70,200);
+	Linked_list l3(5, 201);
+	lists[0] = l1.head;
+	lists[1] = l2.head;
+	lists[2] = l3.head;
+	Solution solution;
+	ListNode *list = solution.mergeKLists(lists);
+	print(list);
+	/* delete l1.head;
+	delete l2.head;
+	delete l3.head;
+
+	delete list; */
+	return 0;
+}
