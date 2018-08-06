1dbb61cb27f78a1beffbf3b29b8a39898f59695a
shintaroDesu <snkraishin87@gmail.com>
2018-08-06 10:51:42 -0400

    fix error

--- substringconcatenation.cpp
+++ substringconcatenation.cpp
@@ -4,18 +4,26 @@
 
 using namespace std;
 
+void printVector(vector<string> s, int size) {
+	for(int x = 0; x < size; x++) {
+		cout << s[x] << " ";
+	}
+	cout << "\n";
+}
+
 class Solution {
 public:
-	int generate(vector<string> &words, vector<string> &combination, string currentString = "",int x = 0, int currentIndex = 0) {
-		if(x >= words.size()) {
-			combination[currentIndex] = currentString;
-			currentIndex++;
-			return currentIndex;
+	void generate(vector<string> words, vector<string> &combination, int vectorsize, string currentString = "") {
+		if(vectorsize == 0) {
+			combination.push_back(currentString);
 		}
-		for(int y = 0; y < words.size(); y++) {
-			currentIndex = generate(words, combination, currentString + words[y], x + 1, currentIndex);
+		for(int y = 0; y < vectorsize; y++) {
+			string temp = words[y];
+			words[y] = words[vectorsize - 1];
+
+			generate(words, combination, vectorsize - 1, currentString + temp);
+			words[y] = temp;
 		}
-		return currentIndex;
 	}
 
 	int in(string s, const vector<string> &combo) {
@@ -27,12 +35,11 @@ public:
 	}
 
 	vector<int> findSubstring(string s, vector<string>& words) {
-		vector<string> combination (pow(words.size(), words.size()));
+		vector<string> combination;
 		vector<int> result;
-		generate(words, combination);
-		int size = combination[words.size() - 1].length();
+		generate(words, combination, words.size());
+		int size = combination[0].length();
 		for(int x = 0; x < s.length() - size + 1; x++) {
-			cout << "substrin: " << s.substr(x, size) << "\n";
 			if(in(s.substr(x, size), combination)) {
 				result.push_back(x);
 				x += size - 1;
@@ -43,8 +50,8 @@ public:
 };
 int main() {
 	Solution solution;
-	vector<string> s = {"words", "students"};
-	string sh = "mywordsstudentsisstudentswords";
+	vector<string> s = {"foo", "bar"};
+	string sh = "barfoothefoobarman";
 	vector<int> shit = solution.findSubstring(sh, s);
 	cout << shit[1] << "\n";
 	return 0;
